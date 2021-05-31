0176fb76baadb31331bfe00c84d84ba4fe39030e
Jaime Guillen <guns-jimi@hotmail.com>
2021-05-31 15:18:57 -0500

    add new numbers

--- Index.js
+++ Index.js
@@ -2,9 +2,9 @@ let Sumar= require('./Sumar');
 let Restar= require('./Restar');
 let Dividir= require('./Dividir');
 let Multiplicar=require('./Multiplicar');
-console.log(Sumar(2,5));
-console.log(Restar(5,6));
-console.log(Dividir(0,9));
+console.log(Sumar(4,5));
+console.log(Restar(5,7));
+console.log(Dividir(0,8));
 console.log(Multiplicar(0,6));
 
 
