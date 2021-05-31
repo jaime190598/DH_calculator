b65cd9e3b9bfc5ecef2b559aa719e95af51d8ff6
Jaime Guillen <guns-jimi@hotmail.com>
2021-05-31 14:44:44 -0500

    mi primer commit

---
+++ Index.js
@@ -0,0 +1,10 @@
+let Sumar= require('./Sumar');
+let Restar= require('./Restar');
+let Dividir= require('./Dividir');
+let Multiplicar=require('./Multiplicar');
+console.log(Sumar(2,5));
+console.log(Restar(5,6));
+console.log(Dividir(0,9));
+console.log(Multiplicar(0,6));
+
+
