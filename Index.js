1c304cbe763a6df962b97951371852ae3b80440e
Jaime Guillen <guns-jimi@hotmail.com>
2021-06-01 12:05:52 -0500

    sumar nuevo numero

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ let Sumar= require('./Sumar');
 let Restar= require('./Restar');
 let Dividir= require('./Dividir');
 let Multiplicar=require('./Multiplicar');
-console.log(Sumar(4,5));
+console.log(Sumar(6,5));
 console.log(Restar(5,7));
 console.log(Dividir(0,8));
 console.log(Multiplicar(0,6));
